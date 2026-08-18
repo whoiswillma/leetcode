701902cda5242fe55e460313d1343f36765f9fb7
William Ma <whoiswillma@gmail.com>
2026-08-18 02:07:41 +0000

    p1563.ts

--- submissions-ts/vitest.config.ts
+++ submissions-ts/vitest.config.ts
@@ -3,5 +3,12 @@ import { defineConfig } from "vitest/config";
 export default defineConfig({
   test: {
     include: ["**/p*.ts"],
+    pool: "threads",
+    poolOptions: {
+      threads: {
+        execArgv: ["--cpu-prof", "--cpu-prof-dir=test-runner-profile"],
+        singleThread: true,
+      },
+    },
   },
 });
